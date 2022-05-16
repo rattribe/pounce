8890a30306704f653eb40856a5dc6edd85abcc23
rattribe <72238082+rattribe@users.noreply.github.com>
2022-05-16 10:56:12 +1000

    Create pounce.sh

---
+++ pounce.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run as sudo!!
+if [[ $EUID -ne 0 ]]
+then
+  echo "Please run as root"
+  exit 1
+fi
+
+# Get Github credentials and logged in user vars
+GHUSER=$(cat github_keys | cut -d: -f1)
+GHTOKEN=$(cat github_keys | cut -d: f2)
+export LIUSER=$(cat /etc/passwd | grep 1000 | cut -d: -f1)
+
+# Clone the rest of the repo to working directory
+cd /tmp
+git clone https://$GHUSER:$GHTOKEN@github.com/$GHUSER/kickstart.git
+
+# Set per
+chown root:$LIUSER /tmp/kickstart/main.sh
+chmod 4754 /tmp/kickstart/main.sh
+
+/bin/bash /tmp/kickstart/main.sh
